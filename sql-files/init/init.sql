36da6d0adbb8652bd73755b455f52d40738f36ce
Thomas <thomas.pellan@plebicom.com>
2020-02-20 18:04:09 +0100

    PB8-Starting data importer example

--- sql-files/init/init.sql
+++ sql-files/init/init.sql
@@ -2,15 +2,17 @@
 CREATE TABLE brand (
        id INT AUTO_INCREMENT NOT NULL,
        name VARCHAR (50) NOT NULL,
-       PRIMARY KEY (id)
+       PRIMARY KEY (id),
+       UNIQUE KEY(name)
 ) ENGINE=INNODB;
 
 CREATE TABLE article (
      id INT AUTO_INCREMENT NOT NULL,
      name VARCHAR (50) NOT NULL,
-     brand INT,
+     brand INT,s
      description VARCHAR(255) DEFAULT NULL,
      PRIMARY KEY (id),
+     UNIQUE KEY(name),
      FOREIGN KEY (brand) REFERENCES brand(id)
 ) ENGINE=INNODB;
 
@@ -19,5 +21,20 @@ CREATE TABLE user (
       username VARCHAR (100) NOT NULL,
       password VARCHAR (250) NOT NULL,
       active BOOLEAN DEFAULT FALSE,
-      PRIMARY KEY (id)
+      PRIMARY KEY (id),
+      UNIQUE KEY(username)
 ) ENGINE=INNODB;
+
+CREATE TABLE configuration (
+      id INT AUTO_INCREMENT NOT NULL,
+      property_key VARCHAR (100) NOT NULL,
+      property_value VARCHAR (250) NOT NULL,
+      PRIMARY KEY (id),
+      UNIQUE KEY(property_key)
+) ENGINE=INNODB;
+
+-- Init with mandatory values for this to work
+INSERT IGNORE INTO configuration (property_key, property_value)
+VALUES ("openFood_url_list" , "https://static.openfoodfacts.org/data/delta/index.txt");
+INSERT IGNORE INTO configuration (property_key, property_value)
+VALUES ("openFood_url_file_query" , "https://static.openfoodfacts.org/data/delta/");
---
+++ src/main/java/com/plebicom/persistence/entity/Configuration.java
@@ -0,0 +1,25 @@
+package com.plebicom.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Configuration {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+	
+	@Column(name = "property_key", unique = true)
+    private String key;
+
+    @Column(name = "property_value", unique = true)
+    private String value;
+}
